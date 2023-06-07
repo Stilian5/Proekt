c116de7682a07c969db6aa5c8a2187ce3295bfc9
Stilian5 <120708857+Stilian5@users.noreply.github.com>
2023-06-07 09:05:21 +0300

    Add files via upload

---
+++ Stilqn-Denis-6ti-Proekt/Stilqn-Denis-6ti-Proekt/Migrations/202306070542283_PriutKucheta.cs
@@ -0,0 +1,43 @@
+﻿namespace Stilqn_Denis_6ti_Proekt.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PriutKucheta : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Animals",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Description = c.String(),
+                        Age = c.Int(nullable: false),
+                        BreedId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Breeds", t => t.BreedId, cascadeDelete: true)
+                .Index(t => t.BreedId);
+            
+            CreateTable(
+                "dbo.Breeds",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Animals", "BreedId", "dbo.Breeds");
+            DropIndex("dbo.Animals", new[] { "BreedId" });
+            DropTable("dbo.Breeds");
+            DropTable("dbo.Animals");
+        }
+    }
+}
---
+++ Stilqn-Denis-6ti-Proekt/Stilqn-Denis-6ti-Proekt/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Stilqn_Denis_6ti_Proekt.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Stilqn_Denis_6ti_Proekt.Model.PriutKuchetaDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Stilqn_Denis_6ti_Proekt.Model.PriutKuchetaDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
